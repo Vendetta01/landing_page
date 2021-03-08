6d946b15e6997c5f1b888d9b2513caa16e00394a
npodewitz <nils.podewitz@googlemail.com>
2021-03-08 22:43:21 +0100

    Icons added to config and page.

--- src/landing_page.py
+++ src/landing_page.py
@@ -1,3 +1,4 @@
+from os import getenv
 from flask import Flask, render_template
 import docker
 
@@ -10,7 +11,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/<page_name>")
 def show_landing_page(page_name="/"):
-    pages = get_container_urls("VIRTUAL_HOST")
+    pages = get_container_urls()
     selected_page = None
     if page_name:
         for page in pages:
@@ -23,16 +24,25 @@ def show_landing_page(page_name="/"):
     return render_template("index.html", pages=pages, selected_page=selected_page)
 
 
-def get_container_urls(env_key):
+def get_container_urls():
     client = docker.from_env()
     container_urls = []
+    blacklist_names = getenv("BLACKLIST_NAMES", "").split(",")
 
     for container in client.containers.list():
         name = container.attrs["Name"][1:]
+        container_dict = {"name": name}
+        if name in blacklist_names:
+            continue
+
         for env_var in container.attrs["Config"].get("Env", []):
-            _env_key, _env_val = env_var.split("=", 1)
-            if _env_key == env_key:
-                container_urls.append({"name": name, "url": _env_val})
+            env_key, env_val = env_var.split("=", 1)
+            if env_key == "VIRTUAL_HOST" and getenv("VIRTUAL_HOST") != env_val:
+                container_dict["url"] = env_val
+            elif env_key == "VIRTUAL_ICON":
+                container_dict["icon"] = env_val
+        if "url" in container_dict:
+            container_urls.append(container_dict)
 
     return container_urls
 
